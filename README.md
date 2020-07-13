527beb8418c5b8da4b231700407f79d7c02f7704
gipsh <gipshm@gmail.com>
2020-07-13 17:17:36 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # aws-epub2kindle
 serverless convert and send epubs to your kindle 
+
+![alt text](https://github.com/gipsh/aws-epub2kindle/blob/master/docs/arq.png?raw=true)
+
---
+++ resources/calibre-layer/build.sh
@@ -0,0 +1,22 @@
+
+#!/bin/bash
+mkdir -p layer
+cd layer
+rm -rf *
+# will download the lastest release
+curl -s https://api.github.com/repos/kovidgoyal/calibre/releases/latest \
+| grep "browser_download_url.*64.txz" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi -
+
+
+tar -xf calibre-*.txz
+rm calibre-*.txz
+# remove some files to hit the 250MB layer limit 
+rm lib/libQt5WebEngineCore.so.5
+rm lib/python2.7/site-packages/calibre/gui2/viewer/highlights.pyo
+rm resources/builtin_recipes.zip
+rm resources/content-server/index-generated.html
+rm resources/localization/locales.zip
+
